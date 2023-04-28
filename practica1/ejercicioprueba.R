942d7c10f936b99504cd2e8e5b2a722fef858fcc
Manoloserranoo <manuel.serranovallejo@usp.ceu.es>
2023-04-28 11:29:43 +0200

    cambios

---
+++ practica1/ejercicioprueba.R
@@ -0,0 +1,20 @@
+df <- data.frame(
+    Mes = c("Enero", "Febrero", "Marzo"),
+    Ingresos = c(45000, 41500, 51200),
+    Gastos = c(33400, 35400, 35600)
+    )
+df 
+
+df$Impuestos <- c(6450, 6300, 7100)
+df
+
+df <- rbind(df, list(Mes = "Abril", Ingresos = 49700, Gastos = 36300, Impuestos = 6850))
+df
+
+df[3, "Ingresos"] <- 50400
+#imprimir el data frame
+df
+
+
+
+
